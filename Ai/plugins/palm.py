7bd7c366e06753dfbf6d8b0ea9a1e4b7f9a6690e
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-03-24 19:12:10 +0530

    Update palm.py

--- Ai/plugins/palm.py
+++ Ai/plugins/palm.py
@@ -20,9 +20,8 @@ def fetch_data(api_url: str, query: str) -> tuple:
     except Exception as e:
         return None, f"An error occurred: {str(e)}"
 
-@bot.on_message(filters.command(["palm"]))
-@bot.on_message(filters.regex("palm"))
-async def chatgpt5(_: Client, message: Message):
+@bot.on_message(filters.command(["palm"]) & filters.regex("palm"))
+async def palm(_: Client, message: Message):
     if len(message.command) < 2:
         return await message.reply_text("**Please provide a query.**")
 
