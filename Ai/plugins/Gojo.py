546812581df5218a137f85630c975eda00434c67
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-03-24 23:00:37 +0530

    Update Gojo.py

--- Ai/plugins/Gojo.py
+++ Ai/plugins/Gojo.py
@@ -2,6 +2,7 @@ import requests
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from Ai import bot
+import asyncio
 
 api_url_chat5 = "https://tofu-api.onrender.com/chat/gpt"
 
@@ -33,12 +34,21 @@ async def gojo_ai(_: Client, message: Message):
     api_response, error_message = fetch_data(api_url_chat5, query, user_id)
     old_prompt[user_id] = api_response
     await message.reply_text(api_response or error_message)
+    await asyncio.sleep(300)  # Clear data after 5 minutes
+    if user_id in old_prompt:
+        del old_prompt[user_id]
 
 @bot.on_message(filters.reply & filters.text & ~filters.me)
 async def reply_to_bot_message(client: Client, message: Message):
     user_id = message.from_user.id
     if user_id in old_prompt:
-        new_query = old_prompt[user_id] + " " + message.text
+        if old_prompt[user_id] is not None:
+            new_query = old_prompt[user_id] + " " + message.text
+        else:
+            new_query = message.text
         api_response, error_message = fetch_data(api_url_chat5, new_query, user_id)
         old_prompt[user_id] = api_response
         await message.reply_text(api_response or error_message)
+        await asyncio.sleep(300)  # Clear data after 5 minutes
+        if user_id in old_prompt:
+            del old_prompt[user_id]
