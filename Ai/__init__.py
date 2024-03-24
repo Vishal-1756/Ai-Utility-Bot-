f2e93f1a24e002ae9c3dab4bf9bab5ee0c59d4c8
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-03-24 17:54:47 +0530

    Create __init__.py

---
+++ Ai/__init__.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from pyrogram import Client 
+
+FORMAT = "[INFO]: %(message)s"
+
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
+              logging.StreamHandler()], format=FORMAT)
+
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+TOKEN = os.getenv("TOKEN")
+GROUP_ID = -1001717881477
+
+bot = Client(
+       name="AI",
+       api_id=API_ID,
+       api_hash=API_HASH,
+       bot_token=TOKEN,
+       plugins=dict(root="Ai"),)
