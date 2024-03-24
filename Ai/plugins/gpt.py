472526f1be0b786489c8344d17f884d6bbc5fdad
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-03-24 20:13:49 +0530

    Update gpt.py

--- Ai/plugins/gpt.py
+++ Ai/plugins/gpt.py
@@ -20,8 +20,8 @@ def fetch_data(api_url: str, query: str) -> tuple:
     except Exception as e:
         return None, f"An error occurred: {str(e)}"
 
-@bot.on_message(filters.command(["chatgpt","gpt"]) & filters.regex(r"gpt"))
-@bot.on_message(filters.command(["chatgpt","gpt"], prefixes="") & filters.regex(r"gpt"))
+@bot.on_message(filters.command(["chatgpt","gpt"]))
+@bot.on_message(filters.command(["chatgpt","gpt"], prefixes=""))
 async def chatgpt5(_: Client, message: Message):
     if len(message.command) < 2:
         return await message.reply_text("**Please provide a query.**")
