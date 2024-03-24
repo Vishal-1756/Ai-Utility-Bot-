f436b2a60b481b1aed4fe2fff17adb74ac355a1d
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-03-24 18:04:39 +0530

    Update __main__.py

--- Ai/__main__.py
+++ Ai/__main__.py
@@ -1,16 +1,22 @@
 from Ai import bot , barath
- 
+import time
+
+start_time = time.time()    
+end_time = time.time()
+ping_time = round((end_time - start_time) * 1000, 3)
+uptime = get_readable_time((time.time() - StartTime))
+    
 RESTART_TEXT = """
 Heya {} I Back To Work Again
 Date :- {}
 Time :- {}
-Uptime :- {}
-Ping {}
 """
+
+async def get_date_time:
 
 async def run_clients():
       await bot.start()
-      zone = await get_datetime()
+      zone = await get_date_time
       await bot.send_message(
            chat_id=config.GROUP_ID,
            text=strings.RESTART_TEXT.format(date=zone["date"], time=zone["time"]))      
