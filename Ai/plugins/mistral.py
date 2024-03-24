9894dfa24d7f7d392c57a9a041b6581463b48e88
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-03-24 19:02:02 +0530

    Update mistral.py

--- Ai/plugins/mistral.py
+++ Ai/plugins/mistral.py
@@ -20,9 +20,8 @@ def fetch_data(api_url: str, query: str) -> tuple:
     except Exception as e:
         return None, f"An error occurred: {str(e)}"
 
-@bot.on_message(filters.command(["mistral"]))
-@bot.on_message(filters.regex("mistral"))
-async def chatgpt5(_: Client, message: Message):
+@bot.on_message(filters.command(["mistral"]) & filters.regex("mistral"))
+async def mistral(_: Client, message: Message):
     if len(message.command) < 2:
         return await message.reply_text("**Please provide a query.**")
 
